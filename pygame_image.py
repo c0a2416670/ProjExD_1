9143013360f66648f93c711714c64c25a2ece162
c0a24166 <c0a2416670@edu.teu.ac.jp>
2025-04-15 15:39:49 +0900

    練習２と４：こうかとん描画

--- pygame_image.py
+++ pygame_image.py
@@ -11,6 +11,8 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("../fig/pg_bg.jpg")
     kk_img = pg.image.load("../fig/3.png") #練習１
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_img = pg.transform.rotozoom(kk_img, 10,1.0)
     tmr = 0
     while True:
         for event in pg.event.get():
