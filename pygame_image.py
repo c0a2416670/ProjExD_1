efbc1d482b686fec4b2e3846ce6a0ced19c3d374
c0a24166 <c0a2416670@edu.teu.ac.jp>
2025-04-15 16:13:16 +0900

    練習９：背景のループ

--- pygame_image.py
+++ pygame_image.py
@@ -20,10 +20,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
-        x = tmr
+        x = tmr%3200 #練習６ 練習９
         screen.blit(bg_img, [-x, 0]) #練習６
         screen.blit(bg_img2, [-x+1600 ,0])#練習７
-        screen.blit(kk_img, [300, 200])
+        screen.blit(bg_img, [-x+3200, 0])#練習９
+        screen.blit(kk_img, [300, 200])#練習４
         pg.display.update()
         tmr += 1        
         clock.tick(200)#練習５
